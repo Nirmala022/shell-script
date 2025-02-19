faa0104d6556c0a23136e2b70debb6634e694e13
nirmala <nirmalamurahari505@gmail.com>
2025-02-19 23:36:21 +0530

    installmysql added

--- 15-loops.sh
+++ 15-loops.sh
@@ -7,13 +7,13 @@ N="\e[0m"
 
 LOGS_FOLDER="/var/log/shellscript-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
-TIMESTAMP=$(date +%Y-%M-%D-%H-%M-%S)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e"$2 ... $R failuere $N"
+        echo -e "$2 ... $R failuere $N"
         exit 1
     else 
         echo -e "$2.... $G success $N"
@@ -32,10 +32,9 @@ do
     dnf list installed $package &>>$LOG_FILE_NAME
     if [ $? -ne 0]
     then 
-          dnf installed $package -y &>>$LOG_FILE_NAME
-          VALIDATE $? "insatalling $package"
+        dnf installed $package -y &>>$LOG_FILE_NAME
+        VALIDATE $? "insatalling $package"
     else
-          echo -e "$package is already $Y ...installed $N"
-    fi    
-     
+        echo -e "$package is already $Y ...installed $N"
+    fi        
 done        
