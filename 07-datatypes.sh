1b71fb30a9431bd1c0e949208ebc86f448071608
nirmala <nirmalamurahari505@gmail.com>
2025-02-19 08:20:13 +0530

    dataypes added

---
+++ 07-datatypes.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MOVIES=("pushpa","rrr","devara")
+
+echo "first movie:${MOVIES[0]}"
+echo "second movie:${MOVIES[1]}"
+echo "third movie:${MOVIES[3]}"
+
+echo "all movies:${MOVIES[@]}"
