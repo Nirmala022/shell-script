4aa10ad5580ecf936107e54df9d69b40eac2a712
nirmala <nirmalamurahari505@gmail.com>
2025-02-19 22:13:40 +0530

    installmysql added

---
+++ 15-loops.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+USER=$(id -u)
+R="\e[32m"
+G="\e[33m"
+Y="\e[34m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%M-%D-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e"$2 ... $R failuere"
+    else 
+        echo "$2....success"
+    fi
+}
+echo "script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+if [ $USERID -ne 0 ]
+then
+   echo "ERROR:: you must have sudo access to execute this script"
+   exit 1
+fi 
+
+for package in $@
+do
+   dnf list installed $package &>>LOG_FILE_NAME
+   
+   if [ $? -ne 0]
+   then 
+     dnf installed $package -y
+     VALIDATE $? "insatalling $package"
+   else
+     echo -e "$package is already $Y ...installed $N
+   fi
+done        
