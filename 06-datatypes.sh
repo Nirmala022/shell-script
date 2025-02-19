21f7e820d0656dbc076b55ba90198e350de75fc8
nirmala <nirmalamurahari505@gmail.com>
2025-02-19 08:07:15 +0530

    dataypes added

--- 06-datatypes.sh
+++ 06-datatypes.sh
@@ -6,4 +6,4 @@ NUMBER2=$2
 TIMESTAMP=$(date)
 echo "script executed at: $TIMESTAMP"
 SUM=$(($NUMBER1+$NUMBER2))
-echo "sum of $NUMBER1 and $NUMBER2 is:$sum"
+echo "sum of $NUMBER1 and $NUMBER2 is: $SUM"
