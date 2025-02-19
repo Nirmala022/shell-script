013fd1bfce5a7b0aa4e73b55ba25e7ecad605ead
nirmala <nirmalamurahari505@gmail.com>
2025-02-19 08:27:47 +0530

    dataypes added

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -2,8 +2,8 @@
 
 MOVIES=("pushpa","rrr","devara")
 
-echo "first movie: ${MOVIES[0]}"
-echo "second movie: ${MOVIES[1]}"
-echo "third movie: ${MOVIES[2]}"
+echo "First movie: ${MOVIES[0]}"
+echo "Second movie: ${MOVIES[1]}"
+echo "Third movie: ${MOVIES[2]}"
 
-echo "all movies: ${MOVIES[@]}"
+echo "All movies are: ${MOVIES[@]}"
