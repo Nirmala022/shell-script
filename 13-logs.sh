86ebbe57d0481e7b39118d51858ff7a967db1ed9
nirmala <nirmalamurahari505@gmail.com>
2025-02-19 21:53:48 +0530

    installmysql added

--- 13-logs.sh
+++ 13-logs.sh
@@ -9,6 +9,7 @@ LOGS_FOLDER="/var/log/shellscript-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%M-%D-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
