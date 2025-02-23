ff5bd8eceafb325df5f01f28b2136c7304e9951f
nirmala <nirmalamurahari505@gmail.com>
2025-02-23 21:59:11 +0530

    expense-shell

--- 18-backup.sh
+++ 18-backup.sh
@@ -9,7 +9,7 @@ SOURCE_DIR=$1
 DEST_DIR=$2
 DAYS=${3:-14}  # IF USER IS NOT PROVIDING NUMBER OF DAYS, WE ARE TAKING 14 AS DEFAULT
 
-LOGS_FOLDER="/var/log/expense-logs"
+LOGS_FOLDER="/home/ec2-user/shellscript-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
@@ -26,6 +26,7 @@ VALIDATE(){
 
 USAGE(){
     echo -e "$R USAGE:: $N sh 18-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
+    exit 1
 }
 
 if [ $# -lt 2]
