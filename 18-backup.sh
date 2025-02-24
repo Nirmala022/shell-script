20028dcaefdde66674a995a0fe4d66bbc5bf60d4
nirmala <nirmalamurahari505@gmail.com>
2025-02-24 15:29:19 +0530

    expense-shell

--- 18-backup.sh
+++ 18-backup.sh
@@ -10,7 +10,7 @@ DEST_DIR=$2
 DAYS=${3:-14}  # IF USER IS NOT PROVIDING NUMBER OF DAYS, WE ARE TAKING 14 AS DEFAULT
 
 LOGS_FOLDER="/home/ec2-user/shellscript-logs"
-LOG_FILE=$(echo $0 | awk -F "/" '${print $NF}' | cut -d "." -f1)
+LOG_FILE=$(echo $0 | awk -F "/" '{print $NF}' | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
